2548fa871f1959c2da4d18a987aab13ba1b9ed9d
johnson-mcj <musukwajohnson5@gmail.com>
2023-05-31 14:40:03 -0700

    fixing errors

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -11,35 +11,34 @@ class Node:
 
     def __init__(self, data, next_node=None):
         """
-        Initializes a Node object with a given data value and next node.
+        Initializes a Node object with the given data and next_node.
 
         Args:
-            data (int): The data value of the node.
-            next_node (Node): The next node in the linked list.
+            data (int): The data of the node.
+            next_node (Node, optional): The next node in the list. Defaults to None.
 
         Raises:
-            TypeError: If the data is not an integer.
-            TypeError: If the next_node is not None or a Node object.
+            TypeError: If the data is not an integer or if next_node is not a Node object.
         """
         self.__data = None
-        self.data = data
         self.__next_node = None
+        self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
         """
-        Retrieves the data value of the node.
+        Retrieves the data of the node.
 
         Returns:
-            int: The data value of the node.
+            int: The data of the node.
         """
         return self.__data
 
     @data.setter
     def data(self, value):
         """
-        Sets the data value of the node.
+        Sets the data of the node.
 
         Args:
             value (int): The data value to be set.
@@ -54,23 +53,23 @@ class Node:
     @property
     def next_node(self):
         """
-        Retrieves the next node in the linked list.
+        Retrieves the next node in the list.
 
         Returns:
-            Node: The next node in the linked list.
+            Node: The next node in the list.
         """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
         """
-        Sets the next node in the linked list.
+        Sets the next node in the list.
 
         Args:
             value (Node): The next node to be set.
 
         Raises:
-            TypeError: If the next_node is not None or a Node object.
+            TypeError: If next_node is not a Node object.
         """
         if value is not None and not isinstance(value, Node):
             raise TypeError('next_node must be a Node object')
@@ -90,19 +89,20 @@ class SinglyLinkedList:
 
     def sorted_insert(self, value):
         """
-        Inserts a new Node into the correct sorted position in the linked list.
+        Inserts a new Node into the correct sorted position in the list (increasing order).
 
         Args:
-            value (int): The data value of the new node to be inserted.
+            value (int): The value to be inserted into the list.
         """
         new_node = Node(value)
-
-        if self.head is None or value < self.head.data:
+        if self.head is None:
+            self.head = new_node
+        elif value < self.head.data:
             new_node.next_node = self.head
             self.head = new_node
         else:
             current = self.head
-            while current.next_node is not None and current.next_node.data < value:
+            while current.next_node is not None and value >= current.next_node.data:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
@@ -114,12 +114,9 @@ class SinglyLinkedList:
         Returns:
             str: The string representation of the linked list.
         """
-        if self.head is None:
-            return ""
-
+        result = ""
         current = self.head
-        result = str(current.data)
-        while current.next_node is not None:
+        while current is not None:
+            result += str(current.data) + "\n"
             current = current.next_node
-            result += '\n' + str(current.data)
-        return result
+        return result.rstrip()
