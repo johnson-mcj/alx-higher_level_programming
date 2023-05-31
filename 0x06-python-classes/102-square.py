2d4877c75fc2d586247577bf91bfa9341b2655e1
johnson-mcj <musukwajohnson5@gmail.com>
2023-05-31 14:30:08 -0700

    fixed errors

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,50 +1,54 @@
 #!/usr/bin/python3
-
 """
-This module defines the Square class.
+Module documentation: Square
 """
 
 
 class Square:
     """
-    This class represents a square.
+    Square class represents a square shape.
     """
 
     def __init__(self, size=0):
         """
-        Initializes a Square object with an optional size.
+        Initializes a Square object with a given size.
 
         Args:
-            size (float or int): The size of the square. Default is 0.
+            size (int or float): The size of the square.
+
+        Raises:
+            TypeError: If the size is not a number (int or float).
+            ValueError: If the size is less than 0.
         """
+        self.__size = 0
         self.size = size
 
     @property
     def size(self):
         """
-        Getter method for the size attribute.
+        Retrieves the size of the square.
 
         Returns:
-            float or int: The size of the square.
+            int or float: The size of the square.
         """
         return self.__size
 
     @size.setter
     def size(self, value):
         """
-        Setter method for the size attribute.
+        Sets the size of the square.
 
         Args:
-            value (float or int): The size of the square.
+            value (int or float): The size value to be set.
 
         Raises:
-            TypeError: If value is not a number (float or integer).
-            ValueError: If value is less than 0.
+            TypeError: If the size is not a number (int or float).
+            ValueError: If the size is less than 0.
         """
-        if not isinstance(value, (float, int)):
-            raise TypeError("size must be a number")
+        if type(value) not in [int, float]:
+            raise TypeError('size must be a number')
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         self.__size = value
 
     def area(self):
@@ -52,84 +56,78 @@ class Square:
         Calculates and returns the area of the square.
 
         Returns:
-            float or int: The area of the square.
+            float: The area of the square.
         """
         return self.__size ** 2
 
     def __eq__(self, other):
         """
-        Equality comparison for two squares based on their areas.
+        Implements the equality comparison between two squares.
 
         Args:
-            other (Square): The other square to compare.
+            other (Square): The other square object to compare.
 
         Returns:
             bool: True if the areas are equal, False otherwise.
         """
-        if isinstance(other, Square):
-            return self.area() == other.area()
-        return False
+        return self.area() == other.area()
 
     def __ne__(self, other):
         """
-        Inequality comparison for two squares based on their areas.
+        Implements the inequality comparison between two squares.
 
         Args:
-            other (Square): The other square to compare.
+            other (Square): The other square object to compare.
 
         Returns:
             bool: True if the areas are not equal, False otherwise.
         """
-        return not self.__eq__(other)
+        return self.area() != other.area()
 
     def __gt__(self, other):
         """
-        Greater than comparison for two squares based on their areas.
+        Implements the greater than comparison between two squares.
 
         Args:
-            other (Square): The other square to compare.
+            other (Square): The other square object to compare.
 
         Returns:
             bool: True if the area is greater, False otherwise.
         """
-        if isinstance(other, Square):
-            return self.area() > other.area()
-        raise TypeError("'>': unsupported operand type(s) for Square and {}".format(type(other)))
+        return self.area() > other.area()
 
     def __ge__(self, other):
         """
-        Greater than or equal to comparison for two squares based on their areas.
+        Implements the greater than or equal comparison between two squares.
 
         Args:
-            other (Square): The other square to compare.
+            other (Square): The other square object to compare.
 
         Returns:
-            bool: True if the area is greater or equal, False otherwise.
+            bool: True if the area is greater than or equal, False otherwise.
         """
-        return self.__gt__(other) or self.__eq__(other)
+        return self.area() >= other.area()
 
     def __lt__(self, other):
         """
-        Less than comparison for two squares based on their areas.
+        Implements the less than comparison between two squares.
 
         Args:
-            other (Square): The other square to compare.
+            other (Square): The other square object to compare.
 
         Returns:
             bool: True if the area is less, False otherwise.
         """
-        if isinstance(other, Square):
-            return self.area() < other.area()
-        raise TypeError("'<': unsupported operand type(s) for Square and {}".format(type(other)))
+        return self.area() < other.area()
 
     def __le__(self, other):
         """
-        Less than or equal to comparison for two squares based on their areas.
+        Implements the less than or equal comparison between two squares.
 
         Args:
-            other (Square): The other square to compare.
+            other (Square): The other square object to compare.
 
         Returns:
-            bool: True if the area is less or equal, False otherwise.
+            bool: True if the area is less than or equal, False otherwise.
         """
-        return self.__lt__(
+        return self.area() <= other.area()
