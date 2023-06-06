33f029c4cbd60c41d808732b25a2b8e6cce60044
johnson-mcj <musukwajohnson5@gmail.com>
2023-06-06 12:55:38 -0700

    fixed bugs

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""
+This is a module that contains a class that avoids dynamically created attributes.
+"""
 
 class LockedClass:
+    """A class that prevents dynamically created attributes, except for 'first_name'."""
+
     __slots__ = ['first_name']
 
-    def __setattr__(self, name, value):
-        if name == 'first_name':
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("Cannot create new instance attribute '{}'".format(name))
+    def __init__(self):
+        """Initialization method."""
+        pass
