29825e678cfc7a69f630d51fb14113a340855ea8
johnson-mcj <musukwajohnson5@gmail.com>
2023-06-06 12:51:32 -0700

    a clas that avoids

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-
-This is a module that containts a clas that avoids
-dynmaically created attributes
-
-"""
-
 
 class LockedClass:
     __slots__ = ['first_name']
 
-    def __init__(self):
-        """ Init method """
-        pass
+    def __setattr__(self, name, value):
+        if name == 'first_name':
+            self.__dict__[name] = value
+        else:
+            raise AttributeError("Cannot create new instance attribute '{}'".format(name))
