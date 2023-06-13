c0e6859316038bbd02c4fb4f655428c40a59527c
johnson-mcj <musukwajohnson5@gmail.com>
2023-06-13 08:10:41 -0700

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,57 +1,45 @@
 #!/usr/bin/python3
-""" Module to print status code """
+"""
+Script that reads stdin line by line and computes metrics
+"""
+
 import sys
 
+metrics = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+total_size = 0
+count = 0
 
-class Magic:
-    """ Class to generates instances with dict and size"""
-    def __init__(self):
-        """ Init method """
-        self.dic = {}
-        self.size = 0
+try:
+    for line in sys.stdin:
+        count += 1
+        split_line = line.split()
+        if len(split_line) >= 7:
+            status_code = split_line[-2]
+            file_size = split_line[-1]
+            if status_code in metrics:
+                metrics[status_code] += 1
+            total_size += int(file_size)
 
-    def init_dic(self):
-        """ Initialize dict """
-        self.dic['200'] = 0
-        self.dic['301'] = 0
-        self.dic['400'] = 0
-        self.dic['401'] = 0
-        self.dic['403'] = 0
-        self.dic['404'] = 0
-        self.dic['405'] = 0
-        self.dic['500'] = 0
+        if count % 10 == 0:
+            print("File size: {:d}".format(total_size))
+            for key in sorted(metrics.keys()):
+                if metrics[key] != 0:
+                    print("{:s}: {:d}".format(key, metrics[key]))
 
-    def add_status_code(self, status):
-        """ add repeated number to the status code """
-        if status in self.dic:
-            self.dic[status] += 1
+except KeyboardInterrupt:
+    pass
 
-    def print_info(self, sig=0, frame=0):
-        """ print status code """
-        print("File size: {:d}".format(self.size))
-        for key in sorted(self.dic.keys()):
-            if self.dic[key] is not 0:
-                print("{}: {:d}".format(key, self.dic[key]))
-
-
-if __name__ == "__main__":
-    magic = Magic()
-    magic.init_dic()
-    nlines = 0
-
-    try:
-        for line in sys.stdin:
-            if nlines % 10 == 0 and nlines is not 0:
-                magic.print_info()
-
-            try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-                magic.size += int(list_line[-1].strip("\n"))
-            except:
-                pass
-            nlines += 1
-    except KeyboardInterrupt:
-        magic.print_info()
-        raise
-    magic.print_info()
+finally:
+    print("File size: {:d}".format(total_size))
+    for key in sorted(metrics.keys()):
+        if metrics[key] != 0:
+            print("{:s}: {:d}".format(key, metrics[key]))
