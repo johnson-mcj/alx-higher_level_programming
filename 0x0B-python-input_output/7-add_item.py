052c830e36f10817a8f7568c21f1ebbd1d328602
johnson-mcj <musukwajohnson5@gmail.com>
2023-06-13 07:49:29 -0700

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-""" Module that adds all arguments to a Python list, and then
-save them to a file
 """
+Defines the add_item function that adds arguments to a list and saves it to a JSON file
+"""
+
 import sys
 import os.path
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+def add_item(args, filename):
+    """
+    Add arguments to a list and save it to a JSON file
 
-my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
+    Args:
+        args (list): List of arguments
+        filename (str): Name of the JSON file
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
+    """
+    if os.path.exists(filename):
+        my_list = load_from_json_file(filename)
+    else:
+        my_list = []
 
-save_file(my_list, "add_item.json")
+    my_list.extend(args)
+    save_to_json_file(my_list, filename)
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    add_item(arguments, "add_item.json")
